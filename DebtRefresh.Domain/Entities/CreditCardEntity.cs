b15960916e6799d64c87cf2c6eb9cf7b6693a7f3
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-02-17 16:09:41 -0800

    Added credit card rewards.

--- DebtRefresh.Domain/Entities/CreditCardEntity.cs
+++ DebtRefresh.Domain/Entities/CreditCardEntity.cs
@@ -7,15 +7,17 @@ using DebtRefresh.Domain.Enums;
 //TODO: Add rewards
 namespace DebtRefresh.Domain.Entities
 {    
-    public class CreditCardEntity : IConsumerDebt, IRewards
+    public class CreditCardEntity : IConsumerDebt
     {
         public CreditCardEntity(IInterestSchedule rates,
                                 IFeeSchedule fees,
-                                IDueDateSchedule duedates)
+                                IDueDateSchedule duedates,
+                                IRewards rewards)
         {
             InterestRates = rates;
             FeeSchedule = fees;
             DueDateSchedule = duedates;
+            Rewards = rewards;
         }
 
         public string VendorName { get; set; }
@@ -26,8 +28,7 @@ namespace DebtRefresh.Domain.Entities
         public IInterestSchedule InterestRates { get; set; }    
         public IFeeSchedule FeeSchedule { get; set; }
         public IDueDateSchedule DueDateSchedule { get; set; }
-        public int PointsEarned { get; set; }
-        public float DollarAmountPerPointValue { get; set; }
+        public IRewards Rewards { get; set; }        
 
         public float CreditAvailable()
         {
---
+++ DebtRefresh.Domain/Entities/CreditCardRewardsEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DebtRefresh.Domain.Interfaces;
+
+namespace DebtRefresh.Domain.Entities
+{
+    public class CreditCardRewardsEntity : IRewards
+    {
+        public int PointsEarned { get; set; }
+        public float DollarAmountPerPointValue { get; set; }
+    }
+}
