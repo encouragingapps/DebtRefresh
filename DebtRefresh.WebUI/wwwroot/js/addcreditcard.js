54366dc9caf93c873e3500edc9e8ebd3f414c784
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-02-19 22:32:53 -0800

    Added interest table via VueJs

--- DebtRefresh.WebUI/wwwroot/js/addcreditcard.js
+++ DebtRefresh.WebUI/wwwroot/js/addcreditcard.js
@@ -6,9 +6,14 @@
         creditCardType: '',
         interestRates: [
             {
-                interestRate: '',
-                startDate: '',
-                endDate: '',
+                interestRate: '0.30',
+                startDate: '1/1/2020',
+                endDate: '6/30/2020',
+            },
+            {
+                interestRate: '0.60',
+                startDate: '7/1/2020',
+                endDate: '12/31/2020',
             }
         ]
     },
