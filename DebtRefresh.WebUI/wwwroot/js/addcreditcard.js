0f2d74680989e65f1520e0a3dd490d782b69a626
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-02-20 21:57:01 -0800

    Added VueJs push and pop methods for interest rates.

--- DebtRefresh.WebUI/wwwroot/js/addcreditcard.js
+++ DebtRefresh.WebUI/wwwroot/js/addcreditcard.js
@@ -8,20 +8,22 @@
             {
                 interestRate: '0.30',
                 startDate: '1/1/2020',
-                endDate: '6/30/2020',
+                endDate: '6/30/2020'
             },
             {
                 interestRate: '0.60',
                 startDate: '7/1/2020',
-                endDate: '12/31/2020',
+                endDate: '12/31/2020'
             }
         ]
     },
     methods: {
-        addInterestRate: function (event) {
-            
-            alert('Add code to add interest here!')
-            
-        }
+        addInterestRate: function (event) {                   
+            this.interestRates.push({});
+        },
+        removeRow: function (event) {            
+            this.interestRates.pop({});
+        }    
+
     }
 })
